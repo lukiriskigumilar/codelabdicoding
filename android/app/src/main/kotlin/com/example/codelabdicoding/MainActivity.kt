5f745f43d9e902cdf25a7af281cec17f1fa63afa
luki riski <contactlukiriski@gmail.com>
2022-09-05 11:00:31 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/codelabdicoding/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codelabdicoding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/DetailScreen.dart
@@ -0,0 +1,77 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+        body: SafeArea(
+       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 19.0 , right: 2.0),
+                child: const Text(
+                  "Farm house lembang",
+                  textAlign : TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                      Column(
+                        children:  const <Widget>[
+                          Icon(Icons.calendar_today),
+                          SizedBox(height: 5.0,),
+                          Text("open Everyday"),
+                      
+                      ],
+                      ),
+                      Column(
+                        children: const<Widget>[
+                          Icon(Icons.access_time),
+                          SizedBox(height:8.0),
+                          Text("09.00 - 20.00")
+                        ],
+                      ),
+                      Column(
+                        children: const<Widget>[
+                          Icon(Icons.account_balance_wallet),
+                          SizedBox(height:8.0),
+                          Text("Rp 36.000")
+                        ],
+                      ),
+                      
+                  ],
+                ),
+              ),
+              Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text(
+                                   "Berada di jalur utama Bandung-Lembang" +
+                                    "Farm House menjadi objek wisata yang tidak pernah sepi pengunjung." +
+                                    "Selain karena letaknya strategis, kawasan ini juga menghadirkan" +
+                                    " nuansa wisata khas Eropa." +
+                                    "Semua itu diterapkan dalam bentuk spot swafoto Instagramable",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontSize: 16.0),
+                  ),
+              ),
+            ],
+          )
+        ),
+
+   );
+  
+  }
+}
